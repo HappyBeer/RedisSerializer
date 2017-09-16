41d8e3b004fe51a1a53103470389f94dee9a638d
tanyongfang <tanyongfang@iboxpay.com>
2017-09-16 12:58:12 +0800

    [+] 线程安全kryo

--- src/main/java/cn/wonhigh/serilizer/redis/KryoRedisSerializer.java
+++ src/main/java/cn/wonhigh/serilizer/redis/KryoRedisSerializer.java
@@ -3,10 +3,6 @@ package cn.wonhigh.serilizer.redis;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
-
 /**
  * redis 序列化  Kryo 实现
  * 
@@ -16,30 +12,21 @@ import com.esotericsoftware.kryo.io.Output;
  * @copyright wonhigh.cn
  */
 public class KryoRedisSerializer<T> implements RedisSerializer<T> {
-	private final Kryo kryo = new Kryo();
-	
-	public static final int BUFFER_SIZE = 4096;
 
 	@Override
 	public byte[] serialize(T t) throws SerializationException {
 		if (t == null) {
 			return new byte[0];
 		}
-		byte[] buffer = new byte[BUFFER_SIZE];
-		Output output = new Output(buffer,-1);
-		kryo.writeClassAndObject(output, t);
-		return output.toBytes();
+		return KryoUtil.writeObjectToByteArray( t);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public T deserialize(byte[] bytes) throws SerializationException {
 		if(bytes == null || bytes.length == 0) {
 			return null;
 		}
-		Input input = new Input(bytes);
-		T t = (T)kryo.readClassAndObject(input);
-		return t;
+		return KryoUtil.readFromByteArray( bytes);
 	}
 
 }
